881b1d430403a7bdc2582ca40b511857a4809a73
AndrewGalifi <44146370+AndrewGalifi@users.noreply.github.com>
2023-07-22 13:57:39 -0400

    Update MultiAgentSearch.py

--- MultiAgentSearch.py
+++ MultiAgentSearch.py
@@ -1,33 +1,41 @@
 import random
-import networkx as nx
+from GraphGenerator import GraphGenerator
 
-# Create graph with 40 nodes
-G = nx.Graph()
-for i in range(1, 41):
-    G.add_node(i)
 
-# Connect nodes in a loop
-for i in range(1, 40):
-    G.add_edge(i, i+1)
-G.add_edge(40, 1) 
+def main():
+    g, agent, target, shortest_paths = GraphGenerator()
 
-num_edges_added = 0
-while num_edges_added < 10:
-  u = random.randint(1, 40) 
-  v = random.randint(1, 40)
+    print("Enter agent number (0-1) to simulate:")
+    agent_selected = int(input())
 
-  if u != v and not G.has_edge(u, v):
-    G.add_edge(u, v)
-    print(f"Added edge: ({u}, {v})")
-    num_edges_added += 1
+    # step is number of target steps, not necessarily agent steps (agent may not move when target steps into agent)
+    step = 0
+    while agent != target:
+        neighbors = list(g.neighbors(target))
+        next_node = random.choice(neighbors)
+        target = next_node
 
-# Initialize target at random node            
-target = random.randint(1, 40) 
+        match agent_selected:
+            case 0:
+                pass
+            case 1:
+                agent = agentOneModel(agent, target, shortest_paths)
 
-# Target takes a random walk
-for i in range(10):
-    neighbors = list(G.neighbors(target))
-    next_node = random.choice(neighbors)
-    target = next_node
+        step += 1
+        if agent == target:
+            print(f"S{step}:\nAgent found Target at node {target}")
+            break
 
-    print(f"Step {i+1}: Target is at node {target}")
+        # print(f"S{step}: Target is at node {target},\nS{step}: Agent is at node {agent}\n")
+
+
+def agentOneModel(agent, target, sp):
+    agent_path = sp[agent][target]
+
+    if len(agent_path) == 1:
+        return agent_path[0]
+
+    return agent_path[1]
+
+
+main()
