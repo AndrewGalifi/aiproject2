36814c4f1442caeac54ed8812d1195039cd8d069
AndrewGalifi <44146370+AndrewGalifi@users.noreply.github.com>
2023-07-25 18:35:51 -0400

    Update MultiAgentSearch.py

--- MultiAgentSearch.py
+++ MultiAgentSearch.py
@@ -4,25 +4,26 @@ from GraphGenerator import GraphGenerator
 from collections import deque
 import time
 
+
 def main(agent_selected):
     g, agent, target, shortest_paths, observed_node, probabilities = GraphGenerator()
 
     target_movement_range = [1]
 
-    # step is the number of target steps, not necessarily agent steps (agent may not move when target steps into agent)
     step = 0
     while agent != target:
         neighbors = list(g.neighbors(target))
         next_node = random.choice(neighbors)
         target = next_node
+        no_step = False
 
         match agent_selected:
             case 0:
                 pass
             case 1:
-                agent = agentOneModel(agent, target, shortest_paths)
+                agent, no_step = agentOneModel(agent, target, g)
             case 2:
-                agent = agentTwoModel(agent, target, g)
+                agent, no_step = agentTwoModel(agent, target, g, shortest_paths)
             case 3:
                 agent = observed_node
             case 4:
@@ -34,30 +35,43 @@ def main(agent_selected):
             case 7:
                 pass
 
+        if no_step:
+            step -= 1
+
         step += 1
+
         if agent == target:
             return step
 
 
 # bfs
-def agentOneModel(agent, target, sp):
-    agent_path = sp[agent][target]
+def agentOneModel(agent, target, g):
+    agent_path = nx.dijkstra_path(g, agent, target)
+
+    no_step = False
 
     if len(agent_path) == 1:
-        return agent_path[0]
+        no_step = True
+        return agent_path[0], no_step
 
-    return agent_path[1]
+    return agent_path[1], no_step
 
 
 # TODO
 # astar, must beat number of moves
-def agentTwoModel(agent, target, g):
-    agent_path = nx.astar_path(g, agent, target)
+def agentTwoModel(agent, target, g, sp):
+    def distance(a, t):
+        return len(sp[a][t])-1
+
+    agent_path = nx.astar_path(g, agent, target, heuristic=distance)
+
+    no_step = False
 
     if len(agent_path) == 1:
-        return agent_path[0]
+        no_step = True
+        return agent_path[0], no_step
 
-    return agent_path[1]
+    return agent_path[1], no_step
 
 
 def agentFourModel(agent, target, g, probabilities, target_movement_range):
@@ -75,7 +89,7 @@ def agentFourModel(agent, target, g, probabilities, target_movement_range):
 
 # TODO
 # less num of examinations (compare # of examinations between a4 / reduce ways of updating probability
-def agentFiveModel(agent, target, g, probabilities,  target_movement_range):
+def agentFiveModel(agent, target, g, probabilities, target_movement_range):
     node_to_observe = getMostLikelyNode(probabilities)
 
     if node_to_observe == target:
@@ -91,10 +105,10 @@ def agentFiveModel(agent, target, g, probabilities,  target_movement_range):
             probabilities[node] += spreading_probability
 
         probabilities[target] = 0
-    
+
         propagateProbabilities(probabilities, g, target_movement_range[0])
 
-    #if node_to_observe != target:
+    # if node_to_observe != target:
     #    updateProbabilities(probabilities, node_to_observe, g, target_movement_range[0])
     #    propagateProbabilities(probabilities, g, target_movement_range[0])
 
@@ -110,7 +124,7 @@ def agentSixModel(agent, target, g, probabilities, target_movement_range, sp):
         return target
 
     if node_to_observe != target:
-        
+
         spreading_probability = probabilities[target] / target_movement_range[0]
 
         nodes_in_range = getNodesInRange(g, target, target_movement_range[0])
